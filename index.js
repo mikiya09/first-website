f7dd0b3da3dc4bedaa6f301ab367de47f04cd817
JoJo <spacelion121319@gmail.com>
2022-03-25 23:50:03 -0400

    music added for play - js

---
+++ index.js
@@ -0,0 +1,53 @@
+
+
+
+var numberOfMusicButtons = document.querySelectorAll(".music").length;
+
+for (var i = 0; i < numberOfMusicButtons; i++) {
+  document.querySelectorAll(".music")[i].addEventListener("click", function() {
+    
+    var buttonInnerHTML = this.innerHTML;
+  
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+
+function makeSound(key) {
+
+  switch (key) {
+    case "music_one":
+      var moon = new Audio("music/月亮代表我的心-张国荣.mp3");
+      moon.play();
+      break;
+
+    case "music_two":
+      var world = new Audio("music/Louis Armstrong - What A Wonderful World.mp3");
+      world.play();
+      break;
+
+    case "music_three":
+      var aruarian = new Audio("music/Nujabes - Luv_sic part 2 (feat.Shing02).mp3");
+      aruarian.play();
+      break;
+
+    default: console.log(key);
+
+  }
+}
+
+
+
+function buttonAnimation(currentKey) {
+
+  var activation = document.querySelector("." + currentKey);
+
+  activation.classList.add("pressed");
+
+  setTimeout(function() {
+    activation.classList.remove("pressed");
+  }, 100);
+
+}
