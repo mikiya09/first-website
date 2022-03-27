89b3ce3e16db98efea48818b5516d70aa03b94e4
JoJo <spacelion121319@gmail.com>
2022-03-26 23:45:02 -0400

    typewriter animation effect added

--- content/home.js
+++ content/home.js
@@ -2,7 +2,7 @@
 
 
 // typewriter animation
-var messageArray = ["More features will be added to the website soon..."];
+var messageArray = ["more features will be added to the website soon..."];
 var textPosition = 0;
 var speed = 60;
 
